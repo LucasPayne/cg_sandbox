dd002b272a95aae70a6f963b214b94ce2a8c8e89
lucaspayne <lucaspayyne@gmail.com>
2020-07-10 15:02:48 +1200

    CMakeLists.txt

---
+++ src/core.h
@@ -0,0 +1,16 @@
+/*
+    
+*/
+#ifndef CORE_H
+#define CORE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <vector>
+#include <string>
+#include <iostream>
+#include <fstream>
+
+#endif // CORE_H
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include "core.h"
+#include "gl.h"
+
+int main(int argc, char *argv[])
+{
+    printf("hello there\n");
+    return 0;
+}

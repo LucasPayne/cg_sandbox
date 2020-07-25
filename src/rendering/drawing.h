99dccef839a81f3a77ac1d7f51b8ea4e716fac27
lucaspayne <lucaspayyne@gmail.com>
2020-07-25 13:46:35 +1200

    more planning for handling shader data

--- src/rendering/drawing.h
+++ src/rendering/drawing.h
@@ -8,29 +8,43 @@ Drawing submodule of the rendering module.
 #include "resource_model/resource_model.h"
 
 
+
+
+struct PropertySheet {
+    // PropertyLayout *layout;
+    // 
+    // GLuint uniform_buffer_id;
+    // bool in_sync;
+    // std::vector<uint8_t> data;
+
+    // void bind() {
+    //     if (in_sync) return;
+    //     glBindBuffer(GL_UNIFORM_BUFFER, uniform_buffer_id);
+    //     glBufferSubData(GL_UNIFORM_BUFFER, (GLintptr) 0, (GLsizeiptr) layout->uniform_buffer_size, &data[0]);
+    //     glBindBuffer(GL_UNIFORM_BUFFER, 0);
+    //     in_sync = true;
+    // }
+
+    // PropertySheet(PropertyLayout *_layout) : layout{_layout} {
+    //     // Create the uniform buffer object. This is a handle for globally accessible constant data in graphics memory.
+    //     glGenBuffers(1, &uniform_buffer_id);
+    //     glBindBuffer(GL_UNIFORM_BUFFER, uniform_buffer_id);
+    //     data = std::vector<uint8_t>(uniform_buffer_size);
+    //     std::fill(data.begin(), data.end(), 0);
+    //     glBufferData(GL_UNIFORM_BUFFER, (GLsizeiptr) layout->uniform_buffer_size, (const void *) &data[0], GL_DYNAMIC_DRAW);
+    //     in_sync = true;
+    //     glBindBuffer(GL_UNIFORM_BUFFER, 0);
+    // }
+};
+
+
+
 template <typename T>
 struct GMSMInstance {
     Resource<T> base;
-    void bind() {
-        if (in_sync) return;
-        glBindBuffer(GL_UNIFORM_BUFFER, uniform_buffer_id);
-        glBufferSubData(GL_UNIFORM_BUFFER, (GLintptr) 0, (GLsizeiptr) uniform_buffer_size, &data[0]);
-        glBindBuffer(GL_UNIFORM_BUFFER, 0);
-        in_sync = true;
-    }
-
-    GLuint uniform_buffer_id;
-    size_t uniform_buffer_size;
-
-    bool in_sync;
-    std::vector<uint8_t> data;
-
-    GMSMInstance(Resource<T> _base) : base{_base} {
-        // Create the uniform buffer object. This is a handle for globally accessible constant data in graphics memory.
-        glGenBuffers
-    }
+    PropertySheet property_sheet;
+    GMSMInstance(Resource<T> _base) : base{_base} {}
 };
-
 struct GeometricMaterialInstance : public GMSMInstance<GeometricMaterial> {
     Resource<VertexArray> vertex_array;
 
@@ -38,7 +52,6 @@ struct GeometricMaterialInstance : public GMSMInstance<GeometricMaterial> {
         GMSMInstance<GeometricMaterial>(_base), vertex_array{_vertex_array}
     {}
 };
-
 struct MaterialInstance : public GMSMInstance<Material> {
     MaterialInstance(Resource<Material> _base) :
         GMSMInstance<Material>(_base)

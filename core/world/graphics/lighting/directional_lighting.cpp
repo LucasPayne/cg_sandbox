1a197e272a7a25461ceb16a3840f7472ec6f2439
Lucas Connor power Payne <lcp35@cs16029fp.canterbury.ac.nz>
2020-12-07 15:04:06 +1300

    syncing

--- core/world/graphics/lighting/directional_lighting.cpp
+++ core/world/graphics/lighting/directional_lighting.cpp
@@ -4,7 +4,7 @@ void Graphics::update_directional_lights()
     /*--------------------------------------------------------------------------------
         Directional lights
     --------------------------------------------------------------------------------*/
-    auto shadow_map_sm = shading.shading_models.load("shaders/shadows/directional_light_shadow_map.sm");
+    auto shadow_map_sm = shading.shading_models.load("shaders/shadows/shadow_map.sm");
     auto shadow_map_shading_model = ShadingModelInstance(shadow_map_sm);
 
     // Render shadow maps.
--- core/world/graphics/lighting/point_lighting.cpp
+++ core/world/graphics/lighting/point_lighting.cpp
@@ -8,7 +8,8 @@ END_ENTRIES()
 
 void Graphics::update_point_lights()
 {
-
+    auto shadow_map_sm = shading.shading_models.load("shaders/shadows/shadow_map.sm");
+    auto shadow_map_shading_model = ShadingModelInstance(shadow_map_sm);
 }
 
 void Graphics::point_lighting(Aspect<Camera> camera)

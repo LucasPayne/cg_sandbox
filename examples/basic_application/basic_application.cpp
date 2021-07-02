c7ad243892cb8534d48828eb22ccd7c356ee55ea
lucaspayne <lucaspayyne@gmail.com>
2021-07-03 11:59:09 +1200

    basic libraries are now working together

--- examples/basic_application/basic_application.cpp
+++ examples/basic_application/basic_application.cpp
@@ -1,4 +1,5 @@
 #include "cg_sandbox.h"
+#include "mesh_processing/mesh_processing.h"
 
 
 class App : public IGC::Callbacks {
@@ -16,6 +17,18 @@ public:
 
 App::App(World &_world) : world{_world}
 {
+    auto quad = SurfaceMesh();
+    auto v1 = quad.add_vertex();
+    auto v2 = quad.add_vertex();
+    auto v3 = quad.add_vertex();
+    auto v4 = quad.add_vertex();
+    quad.add_triangle(v1, v3, v4);
+    quad.add_triangle(v1, v4, v2);
+    auto grid = Enmesh::grid_mesh(10, 10);
+
+    std::cout << grid.num_vertices() << "\n";
+
+    getchar();
 }
 
 void App::close()

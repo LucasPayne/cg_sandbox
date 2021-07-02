4aa86ae133a1ba586ba4286cb57518ea4a657a1f
lucaspayne <lucaspayyne@gmail.com>
2021-07-03 02:41:04 +1200

    Need to actually link the libraries to example application

--- __new/examples/basic_application/basic_application.cpp
+++ __new/examples/basic_application/basic_application.cpp
@@ -1,11 +1,49 @@
 #include "cg_sandbox.h"
 
+
+class App : public IGC::Callbacks {
+public:
+    World &world;
+    App(World &world);
+
+    void close();
+    void loop();
+    void keyboard_handler(KeyboardEvent e);
+    void mouse_handler(MouseEvent e);
+    void window_handler(WindowEvent e);
+};
+
+
+App::App(World &_world) : world{_world}
+{
+}
+
+void App::close()
+{
+}
+
+void App::loop()
+{
+}
+
+void App::window_handler(WindowEvent e)
+{
+}
+
+void App::keyboard_handler(KeyboardEvent e)
+{
+}
+
+void App::mouse_handler(MouseEvent e)
+{
+}
+
 int main(int argc, char *argv[])
 {
     printf("[main] Creating context...\n");
     IGC::Context context("A world");
     printf("[main] Creating world...\n");
-    World world;
+    World world(context);
     printf("[main] Adding world callbacks...\n");
     context.add_callbacks(&world);
     context.add_callbacks(&world.input);
